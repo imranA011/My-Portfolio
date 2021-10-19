af51c4e7a2e3661efe310218bcde3a94e0f9674c
Imran Ahmed <91880220+imranA011@users.noreply.github.com>
2021-10-19 17:28:03 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const toggleButton =document.getElementsByClassName('toggle-icon')[0];
-const navMenu = document.getElementsByClassName('primary-nav');
+const navMenu = document.getElementsByClassName('primary-menu');
 toggleButton.addEventListener('click', function(){
    for(var i=0; i<navMenu.length; i++)
    navMenu[i].classList.toggle('active');
