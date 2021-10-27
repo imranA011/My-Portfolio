8e0b7d09307133c970d1f2445474487d764c62dc
Imran Ahmed <91880220+imranA011@users.noreply.github.com>
2021-10-28 00:53:05 +0600

    Add files via upload
    update files

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-const toggleButton =document.getElementsByClassName('toggle-icon')[0];
-const navMenu = document.getElementsByClassName('primary-menu');
-toggleButton.addEventListener('click', function(){
-   for(var i=0; i<navMenu.length; i++)
-   navMenu[i].classList.toggle('active');
-})
+// const toggleButton =document.getElementsByClassName('toggle-button')[0];
+// const navMenu = document.getElementsByClassName('primary-menu');
+// toggleButton.addEventListener('click', function(){
+//    for(var i=0; i<navMenu.length; i++)
+//    navMenu[i].classList.toggle('active');
+// })
 
+const toggleButton =document.getElementsByClassName('toggle-button')[0];
+const navMenu = document.getElementsByClassName('primary-menu')[0];
+toggleButton.addEventListener('click', () => {
+   navMenu.classList.toggle('active');
+})
